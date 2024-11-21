d2e1bf8cc0d9b2dd08378eccaa6e6a11fc339317
Lora-Craft <lora-craft@DESKTOP-HS70L85.>
2024-11-21 19:12:59 +1100

    Add character counting dictionary

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
 def main():
-    # opens file and reads its contents
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-    
-    # Prints contents of file
-    print(file_contents)
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    words_num = get_words_num(text)
+    char_nums = get_char_num(text) 
+    print(f"There are {words_num} found in the text")
+    print(char_nums)
 
-# Call the main function to run the program
-if __name__ == "__main__":
-    main()
+def get_words_num(text):
+    words = text.split()
+    return len(words)
+
+
+
+def get_char_num(text):
+    char_dict = {}        
+    for char in text:
+        lower_char = char.lower()
+        if lower_char in char_dict:
+            char_dict[lower_char] += 1
+        else:
+            char_dict[lower_char] = 1
+    return char_dict
+    
+
+    
+    
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+main()
